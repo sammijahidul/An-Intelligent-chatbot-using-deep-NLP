c017f71b0396989faca462405bb657c9fc9e85d4
sammijahidul <jahidul.islam03@northsouth.edu>
2020-09-29 00:59:05 +0600

    created a dictionary from lines dataset

--- botchat.py
+++ botchat.py
@@ -6,7 +6,13 @@ import tensorflow as tf
 import re 
 import time
 
-
 # Importing the dataset
 lines = open('movie_lines.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
 conversations = open('movie_conversations.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
+
+# Creating a dictionary that maps each line and its id
+dic_line = {}
+for line in lines:
+    _line = line.split(' +++$+++ ')
+    if len(_line) == 5:
+        dic_line[_line[0]] = _line[4]
