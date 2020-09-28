214d651fb22fc4ccde1de461956a1142f5d40a59
sammijahidul <jahidul.islam03@northsouth.edu>
2020-09-29 00:29:07 +0600

    importing the dataset

--- botchat.py
+++ botchat.py
@@ -1,6 +1,5 @@
 # Building a Intelkigent chatbot using Deep Nlp
 
-
 # Importing the necessary libraries 
 import numpy as np
 import tensorflow as tf
@@ -8,3 +7,6 @@ import re
 import time
 
 
+# Importing the dataset
+lines = open('movie_lines.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
+conversations = open('movie_conversations.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
