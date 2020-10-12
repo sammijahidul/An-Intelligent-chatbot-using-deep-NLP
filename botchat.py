032b29e470a9ddf29ab0a0e932f6d4e8c4d1700d
sammijahidul <jahidul.islam03@northsouth.edu>
2020-10-12 23:58:11 +0600

    added a fuction for decoding the training set

--- botchat.py
+++ botchat.py
@@ -162,7 +162,8 @@ def encoder_rnn_layer(rnn_input, rnn_size, num_layers, keep_prob, sequence_lengh
                                                        dtype = tf.float32)
     return encoder_state
                                             
-    
+# Decoding the training set
+def decode_training_Set(encoder_state, decoder_cell, decoder_embedded_input):   
        
     
     
